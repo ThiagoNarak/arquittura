fcdffae5b50636924b0adbfa235886de532164c5
thiagoRCOliveira <thiago.workshop.c@gmail.com>
2018-09-22 10:36:59 -0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        
+       Emulator emulator = new Emulator(16,16);
+        System.out.println("dasdada");
+       emulator.run();
+
+    }
+
+
+
+
+
+}
---
+++ src/main2.java
@@ -0,0 +1,12 @@
+import com.sun.prism.paint.Color;
+
+import java.util.Arrays;
+
+public class main2 {
+    public static void main(String[] args) {
+        System.out.println("dasdas");
+        Emulator emulator = new Emulator(16,16);
+        emulator.run();
+
+    }
+}
